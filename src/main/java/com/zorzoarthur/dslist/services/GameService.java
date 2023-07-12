73f848a3db538572faf0a1b423caaa27e0bf4a6d
Arthur Zorzo <arthurcorteszorzo@gmail.com>
2023-07-12 11:41:53 -0300

    Games By List

--- src/main/java/com/zorzoarthur/dslist/services/GameService.java
+++ src/main/java/com/zorzoarthur/dslist/services/GameService.java
@@ -9,6 +9,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.zorzoarthur.dslist.dto.GameDTO;
 import com.zorzoarthur.dslist.dto.GameMinDTO;
 import com.zorzoarthur.dslist.entities.Game;
+import com.zorzoarthur.dslist.projections.GameMinProjection;
 import com.zorzoarthur.dslist.repositories.GameRepository;
 
 @Service
@@ -27,6 +28,13 @@ public class GameService {
     @Transactional(readOnly = true)
     public List<GameMinDTO> findAll() {
         List<Game> result = gameRepository.findAll();
+        List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
+        return dto;
+    }
+
+    @Transactional(readOnly = true)
+    public List<GameMinDTO> findByList(Long listId) {
+        List<GameMinProjection> result = gameRepository.searchByList(listId);
         List<GameMinDTO> dto = result.stream().map(x -> new GameMinDTO(x)).toList();
         return dto;
     }
